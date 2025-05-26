5dae663acb149001553590ee37b661e0d314a0d6
Vikash <ivikash.thakur1@gmail.com>
2025-05-26 20:06:59 +0530

    Added logic to update seconds

--- src/lib/digital_watch.js
+++ src/lib/digital_watch.js
@@ -1,12 +1,23 @@
-import react from "react";
+import { react, useState, useEffect } from "react";
 
-export const DigitalWatch = (
+export const DigitalWatch = () => {
 
-) => {
-    let currentDate = new Date();
-    return <div>
-       
-        {currentDate.getHours()}: {currentDate.getMinutes()} : {currentDate.getSeconds()}
+  let currentDate = new Date();
+  const [sec, setSec] = useState(currentDate.getSeconds());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSec(sec=> sec+1);
+    }, 1000);
 
+    return () => clearInterval(intervalId);
+
+
+    
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: "black", color: "gray", fontSize: "128px" }}>
+      {currentDate.getHours()}: {currentDate.getMinutes()} : {sec}
     </div>
-}
+  );
+};
